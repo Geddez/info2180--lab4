c279b494ef5e1efaf28eb52f9e5da7b48662142e
Daniel Geddes <geddyville@gmail.com>
2018-10-11 17:46:50 -0500

    Question No.1

--- maze.js
+++ maze.js
@@ -1,1 +1,14 @@
-// JavaScript source code
+window.onload = function () {
+    var boundary1 = document.getElementById("boundary1");
+    boundary1.onmouseover = function () {
+        if (!hasClass(boundary1, "youlose")) {
+            boundary1.className += " youlose";
+        }
+
+    }
+
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
