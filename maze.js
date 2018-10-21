58fa79269f628f96737f46237b8276a56ca096ce
Daniel Geddes <geddyville@gmail.com>
2018-10-21 12:25:22 -0500

    Question No. 3

--- maze.js
+++ maze.js
@@ -1,6 +1,10 @@
 var boundaries;
+var end;
+
 window.onload = function () {
     boundaries = document.querySelectorAll(".boundary");
+    end = document.getElementById("end");
+    end.onmouseover = Win;
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].onmouseover = overBounds;
     }
@@ -15,5 +19,11 @@ function overBounds() {
         for (var i = 0; i < boundaries.length; i++) {
             boundaries[i].className += " youlose";
         }
+    }
+}
+
+function Win() {
+    if (!hasClass(boundaries[0], "youlose")) {
+        alert("You Win, Congratulations!!!");
     }
 }
